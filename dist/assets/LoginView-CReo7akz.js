e83f7ddd663a9e7607de9bc8758389a5d4c42739
Julio Cesar Polmann <84574865+Julio-Cesar-PC@users.noreply.github.com>
2025-02-24 22:44:10 -0300

    gh-pages

--- dist/assets/LoginView-CReo7akz.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,a as t,o as n}from"./index-DLWUtDBv.js";const a={},c={class:"about"};function r(l,e){return n(),s("div",c,e[0]||(e[0]=[t("h1",null,null,-1)]))}const i=o(a,[["render",r]]);export{i as default};
