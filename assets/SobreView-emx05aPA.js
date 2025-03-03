33f64ca9e145954aa30ea04e38d90247339a9ab6
Julio Cesar Polmann <84574865+Julio-Cesar-PC@users.noreply.github.com>
2025-03-03 19:17:07 -0300

    Updates

---
+++ assets/SobreView-emx05aPA.js
@@ -0,0 +1,1 @@
+import{c,f as s,o as a}from"./index-CBe5fjYH.js";const l=(o,e)=>{const t=o.__vccOpts||o;for(const[r,n]of e)t[r]=n;return t},f={};function _(o,e){return a(),c("main",null,e[0]||(e[0]=[s("h1",null,"Sobre view",-1)]))}const u=l(f,[["render",_]]);export{u as default};
