3b2f4aafff36c220d65e7567f409f0bd4bc5aac4
Julio Cesar Polmann <84574865+Julio-Cesar-PC@users.noreply.github.com>
2025-03-18 07:52:22 -0300

    Updates

--- assets/SobreView-axrJfUEU.js
+++
@@ -1,1 +0,0 @@
-import{c,g as s,o as a}from"./index-D5v7rmjB.js";const l=(o,e)=>{const t=o.__vccOpts||o;for(const[r,n]of e)t[r]=n;return t},_={};function f(o,e){return a(),c("main",null,e[0]||(e[0]=[s("h1",null,"Sobre view",-1)]))}const u=l(_,[["render",f]]);export{u as default};
